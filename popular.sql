bbf0f17a2bc8e53fc1e75c09d43973ca0b19f115
Emrs <email@gmail.com>
2026-05-10 14:39:13 -0300

    Ajuste: Removendo itens não têxteis e focando apenas em roupas

--- popular.sql
+++ popular.sql
@@ -1,5 +1,5 @@
 ﻿INSERT INTO doacoes (titulo, descricao, tamanho, fk_usuario, fk_categoria, imagem_url) VALUES 
 ('Agasalho de Inverno', 'Casaco de lã azul em ótimo estado', 'G', 1, 1, 'https://images.unsplash.com/photo-1591195853828-11db59a44f6b'),
 ('Tênis Esportivo', 'Tênis para corrida pouco usado', '40', 1, 1, 'https://images.unsplash.com/photo-1542291026-7eec264c27ff'),
-('Cesta Básica', 'Alimentos não perecíveis lacrados', 'Único', 1, 2, 'https://images.unsplash.com/photo-1584132967334-10e028bd69f7'),
-('Cadeira de Rodas', 'Modelo dobrável, revisada recentemente', 'Único', 1, 3, 'https://plus.unsplash.com/premium_photo-1663100722417-6e36673fe0ed');
+('Camisa Polo', 'Camisa branca de algodão, seminova', 'M', 1, 1, 'https://images.unsplash.com/photo-1581655353564-df123a1eb820'),
+('Calça Jeans', 'Calça jeans masculina azul escuro', '42', 1, 1, 'https://images.unsplash.com/photo-1542272604-787c3835535d');
